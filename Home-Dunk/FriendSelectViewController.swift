cd845d5844582874c026d87e31d916507bd72246
Ben Cullen <ben.cullen.n@gmail.com>
2021-01-23 17:05:09 -0700

    Added code for friend select screen

---
+++ Home-Dunk/FriendSelectViewController.swift
@@ -0,0 +1,38 @@
+//
+//  FriendSelectViewController.swift
+//  Home-Dunk
+//
+//  Created by Ben Cullen on 1/23/21.
+//
+
+import Foundation
+import UIKit
+
+class FriendSelectViewController: UITableViewController {
+
+    private var friends: [String] = ["Jimmy"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return friends.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "friendCell")!
+        let friend = friends[indexPath.row]
+        cell.textLabel?.text = "Play \(friend)"
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let friend = friends[indexPath.row]
+        
+        let vc = storyboard?.instantiateViewController(withIdentifier: "LiveGameVC") as! LiveGameViewController
+        vc.opponent = friend
+        show(vc, sender: self)
+    }
+
+}
---
+++ Home-Dunk/LiveGameViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LiveGameViewController.swift
+//  Home-Dunk
+//
+//  Created by Ben Cullen on 1/23/21.
+//
+
+import UIKit
+
+class LiveGameViewController: UIViewController {
+    var opponent = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
